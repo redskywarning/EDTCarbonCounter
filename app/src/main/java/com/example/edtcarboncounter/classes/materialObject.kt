d14919f036be49298b369c3027a50267157d4e1d
redskywarning <sky.alexander@spgs.org>
2024-02-29 22:20:18 +0000

    Classes for Validation + Storing Data

---
+++ app/src/main/java/com/example/edtcarboncounter/classes/materialObject.kt
@@ -0,0 +1,3 @@
+package com.example.edtcarboncounter.classes
+
+class materialObject (val name: String, val mkg: Long, var transports: List<transportObject>)
---
+++ app/src/main/java/com/example/edtcarboncounter/classes/transportObject.kt
@@ -0,0 +1,3 @@
+package com.example.edtcarboncounter.classes
+
+class transportObject (val type: String, val distance: Long)
